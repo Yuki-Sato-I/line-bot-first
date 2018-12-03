f950fcf42a9b828206e9e1f71335bae33ca39ffa
YukiSato <yukiyukiamam@gmail.com>
2018-12-03 09:15:06 +0900

    first commit

--- Gemfile
+++ Gemfile
@@ -64,3 +64,9 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+# ①.env
+gem 'dotenv-rails'
+
+# ②LINE bot
+gem 'line-bot-api'
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,15 @@
 class ApplicationRecord < ActiveRecord::Base
-  self.abstract_class = true
+
+  require_relative 'boot'
+
+  require 'rails/all'
+  Bundler.require(*Rails.groups)
+
+  module Railnotice
+    class Application < Rails::Application
+      config.load_defaults 5.2
+      config.time_zone = 'Tokyo'
+      config.i18n.default_locale = :ja
+  end
+end
 end
